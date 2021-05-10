ebbde7d9fc71e0cd080c0d0e0d168e603dfc5845
Ashwin-Shetye <80038082+Ashwin-Shetye@users.noreply.github.com>
2021-05-10 19:04:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var names_of_people=[];
+function submit()
+{
+    var guess_name=document.getElementById("name1").value
+    names_of_people.push("guess_name");
+    console.log(guess_name);
+    console.log(names_of_people);
+    var lenght_of_name = names_of_people.length; console.log(lenght_of_name);
+     document.getElementById("display_name").innerHTML=names_of_people.toString();
+}
+function sorting()
+{
+    names_of_people.sort();
+    var i=names_of_people.join("<br>");
+    console.log(names_of_people);
+    document.getElementById("sort_button").innerHTML=i.toString();
+}
+function show()
+{
+    var i=names_of_people.join("<br>");
+    console.log(names_of_people);
+    document.getElementById("sort_button").style.display="block";
+}
